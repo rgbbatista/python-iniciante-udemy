b367bfec241e97421c60313f26f100e8e6e1cd6c
Rodrigo Batista <81887045+rgbbatista@users.noreply.github.com>
2021-07-22 11:01:14 -0300

    declarações de funções

---
+++ Funções.py
@@ -0,0 +1,26 @@
+#Funçoes em Python são definidas pela palavra def
+
+def soma(x, y):
+    return x + y
+s = soma(2, 3)
+print(s)
+
+def multiplicacao (x, y):
+    return x * y
+m = multiplicacao(4, 3)
+print(m)
+
+def substracao (x, y):
+    return x - y
+sub = substracao(15, 7)
+print(sub)
+
+def divisao (x, y):
+    return x / y
+d = divisao(4, 2)
+print(d)
+print('\n')
+
+#chamar várias funções recursivamente
+print(soma(d, sub)# soma do resultado da subtração e da divisão
+
